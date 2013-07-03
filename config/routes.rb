0d40914ac0df6f60273282680fa2a6c27922c265
Yuichi Kuroda <pochi.black@gmail.com>
2013-07-03 20:07:51 +0900

    be rails generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Quiz::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +40,7 @@ Quiz::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
@@ -53,4 +54,5 @@ Quiz::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root :to => 'home#index'
 end
